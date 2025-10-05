14d5aac36cf6bf1e5f82160fa004f08959501c51
Reprincev Gleb Dmitrievich IP-513 <gleb_princ@icloud.com>
2025-10-05 22:01:55 +0000

    Исправлено приветствие

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@
 int main(int argc, char** argv) {
     char* str = (char*)malloc(sizeof(char) * SIZE);
     printf("Введите своё имя:\n");
-    str = gets(str);
-    printf("Привет, %s", str);
+    scanf("%s", str);
+    puts("Привет");
+    puts(str);
     return 0;
 }
